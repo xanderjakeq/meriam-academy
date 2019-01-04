169c032242b27dec1e2087a11848f91cafd58eb1
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-01-04 17:02:52 -0500

    V1

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,25 +11,38 @@ $(document).ready(function() {
 $(function() {
     $.scrollify({
         section : ".section",
-
+        offset:1,
         scrollbars:false,
         interstitialSection:".footer",
       before:function(i,panels) {
 
         var ref = panels[i].attr("data-section-name");
-
+        console.log(i)
+        
         $(".nav .active").removeClass("active");
-        $(".nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+        if(i>=1 && i<=3){
+          $(".nav").find("a[href=\"#" + 'about' + "\"]").addClass("active");
+        }else if(i>=4){
+          $(".nav").find("a[href=\"#" + 'contact' + "\"]").addClass("active");
+        }
+        else{
+          $(".nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+        }
+        $(".pagination .active").removeClass("active");
+
+        $(".pagination").find("a[href=\"#" + ref + "\"]").addClass("active");
+      
       },
       afterRender:function() {
         var pagination = "<ul class=\"pagination\">";
         var activeClass = "";
-        $(".panel").each(function(i) {
+        $(".section").each(function(i) {
           activeClass = "";
           if(i===0) {
             activeClass = "active";
           }
-          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+          // <span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span>
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"></a></li>";
         });
 
         pagination += "</ul>";
@@ -44,12 +57,23 @@ $(function() {
         });
 
         */
-        $(".nav a").on("click",$.scrollify.move);
+        $(".nav a, .pagination a").on("click",$.scrollify.move);
       }
     });
 
     var typed = new Typed('#slogan', {
       strings: [ "'Providing Excellent Foundation for your Child's Future' ^1000", "Call us (202) 248-1848"],
-      typeSpeed: 50
+      typeSpeed: 50,
+      loop: true,
+        loopCount: Infinity,
+        backDelay: 5000,
+    });
+
+    var call = new Typed('#visit', {
+      strings: [ "We love our family","so feel free to visit","during normal business hours. ^1000"],
+      typeSpeed: 50,
+      loop: true,
+        loopCount: Infinity,
+        backDelay: 1000,
     });
 });
