516a4d76117983368967978fa2cd36227f003e1f
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-01-04 02:25:00 -0500

    jquery-scrollify installed

---
+++ public/scripts/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $(window).on('scroll', function() {
+      if (Math.round($(window).scrollTop()) > 100) {
+        $('.navbar').addClass('scrolled');
+      } else {
+        $('.navbar').removeClass('scrolled');
+      }
+    });
+  });
+
+$(function() {
+    $.scrollify({
+        section : ".section",
+    });
+});
