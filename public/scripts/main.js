a91e43490611fa656efce697ec7eb8ab5aaaff76
Xander Jake de los Santos <xanderjakeq@gmail.com>
2019-01-04 04:39:29 -0500

    pagination nav

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,5 +11,39 @@ $(document).ready(function() {
 $(function() {
     $.scrollify({
         section : ".section",
+
+        scrollbars:false,
+      before:function(i,panels) {
+
+        var ref = panels[i].attr("data-section-name");
+
+        $(".nav .active").removeClass("active");
+        $(".nav").find("a[href=\"#" + ref + "\"]").addClass("active");
+      },
+      afterRender:function() {
+        var pagination = "<ul class=\"pagination\">";
+        var activeClass = "";
+        $(".panel").each(function(i) {
+          activeClass = "";
+          if(i===0) {
+            activeClass = "active";
+          }
+          pagination += "<li><a class=\"" + activeClass + "\" href=\"#" + $(this).attr("data-section-name") + "\"><span class=\"hover-text\">" + $(this).attr("data-section-name").charAt(0).toUpperCase() + $(this).attr("data-section-name").slice(1) + "</span></a></li>";
+        });
+
+        pagination += "</ul>";
+
+        $(".home").append(pagination);
+        /*
+
+        Tip: The two click events below are the same:
+
+        $(".pagination a").on("click",function() {
+          $.scrollify.move($(this).attr("href"));
+        });
+
+        */
+        $(".nav a").on("click",$.scrollify.move);
+      }
     });
 });
